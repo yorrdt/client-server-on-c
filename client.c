e2b374856765bef87dbb8ced1a5ab9320fb034fc
yorrdt <this.mazahaka@gmail.com>
2022-10-21 01:40:22 +0300

    server output messages

--- client.c
+++ client.c
@@ -13,7 +13,7 @@ int main(int argc, char** argv) {
     int client_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	
 	if (client_socket == -1) {
-		perror("[CE]: error in calling socket()");
+		perror("[ClientError]: error in calling socket()");
 		exit(1);
 	}
 
@@ -23,28 +23,15 @@ int main(int argc, char** argv) {
 	
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(8080);
-    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
 
     if(connect(client_socket, (struct sockaddr*)&server_address, sizeof(server_address)) == -1) {
-		perror("[CE]: connection error");
+		perror("[ClientError]: connection error");
 		close(client_socket);
 		exit(1);
 	}
-	
-	for (;;) {
-		char buffer[1024] = {0};
-		printf("Client: ");
-		fgets(buffer, sizeof(buffer), stdin);
-		send(client_socket, buffer, sizeof(buffer), 0);
-		
-		memset(buffer, 0, sizeof(buffer));
-		
-		recv(client_socket, &buffer, sizeof(buffer), 0);
-		printf("Server: %s", buffer);
-		
-		// sleep(5);
-	}
-	
+
+
 	shutdown(client_socket, SHUT_RDWR);
 	
     close(client_socket);
--- server.c
+++ server.c
@@ -14,7 +14,7 @@ int main(int argc, char** argv) {
     int server_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
     if (server_socket == -1) {
-        perror("[SE]: error in calling socket()");
+        perror("[ServerError]: error in calling socket()");
         exit(1);
     }
 
@@ -24,45 +24,50 @@ int main(int argc, char** argv) {
 	
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(8080);
-    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    char server_ip[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &(server_address.sin_addr), server_ip, INET_ADDRSTRLEN);
+
+    printf("[ServerMessage]: server %s:%d\n", server_ip, ntohs(server_address.sin_port));
 
     if(bind(server_socket, (struct sockaddr*)&server_address, sizeof(server_address)) == -1) {
-        perror("[SE]: bind() call failed");
+        perror("[ServerError]: bind() call failed");
 		close(server_socket);
         exit(1);
     }
 
     if(listen(server_socket, 5) == -1) {
-		perror("[SE]: listen() call failed");
+		perror("[ServerError]: listen() call failed");
 		close(server_socket);
         exit(1);
 	}
-	
-	int client_accept = accept(server_socket, 0, 0);
+
+
+	while (1) {
+
+		struct sockaddr_in client_struct;
+		int client_struct_len = sizeof(client_struct);
+
+		int client_socket = accept(server_socket, (struct sockaddr*)&client_struct, &client_struct_len);
 		
-	if (client_accept < 0) {
-		perror("[SE]: accept() call failed");
-		close(server_socket);
-		exit(1);
+		if (client_socket < 0) {
+			perror("[ServerError]: accept() call failed");
+			close(server_socket);
+			exit(1);
+		}
+
+
+		char client_ip[INET_ADDRSTRLEN];
+	    inet_ntop(AF_INET, &(client_struct.sin_addr), client_ip, INET_ADDRSTRLEN);
+
+		printf("[ServerMessage]: connected client (%s:%d)\n", client_ip, ntohs(client_struct.sin_port));
+		
+
+		shutdown(client_socket, SHUT_RDWR);
+
+		close(client_socket);
 	}
 	
-	for (;;) {
-		char buffer[1024] = {0};
-		recv(client_accept, &buffer, sizeof(buffer), 0);
-		printf("Client: %s", buffer);
-		
-		memset(buffer, 0, sizeof(buffer));
-		
-		printf("Server: ");
-		fgets(buffer, sizeof(buffer), stdin);
-		send(client_accept, buffer, sizeof(buffer), 0);
-
-		// sleep(5);
-	}
-	
-	shutdown(client_accept, SHUT_RDWR);
-
-	close(client_accept);
-
     return 0;
 }
